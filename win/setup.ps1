9e3ed7b898f978808c9253d9b6ddcbac048444ee
Josh Fogus <josh.fogus@gmail.com>
2022-08-15 07:48:18 -0700

    Setup script now installs dependencies.

--- win/setup.ps1
+++ win/setup.ps1
@@ -1,18 +1,16 @@
-# Process elevation from https://docs.microsoft.com/en-us/archive/blogs/virtual_pc_guy/a-self-elevating-powershell-script
 $windowId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$windowPrincipal = new-object System.Security.Principal.WindowsPrincipal($windowId)
+$windowPrincipal = New-Object System.Security.Principal.WindowsPrincipal($windowId)
 
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 if ($windowPrincipal.IsInRole($adminRole)) {
     # Privileges are elevated, ensure screen appearance reflects this
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-    # $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    $Host.UI.RawUI.WindowTitle = $MyInvocation.MyCommand.Definition + "(Elevated)"
     clear-host
 } else {
-    # Privileges are not elevated, elevate them and restart the process
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"
-    $newProcess.arguments = $myInvocation.MyCommand.Definition
-    $newProcess.verb = "runas"
+    # Privileges are not elevated, elevate them and restar the process
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+    $newProcess.Arguments = $MyInvocation.MyCommand.Definition
+    $newProcess.Verb = "runas"
 
     [System.Diagnostics.Process]::Start($newProcess)
     exit
@@ -20,15 +18,14 @@ if ($windowPrincipal.IsInRole($adminRole)) {
 
 # Install Chocolatey
 if (Get-Command choco -ErrorAction SilentlyContinue) {
-    Write-Host "Chocolatey is already installed."
-}
-else {
+    Write-Host "Chocolately is already installed."
+} else {
     $response = ""
     while ($response -eq "") {
-        write-Host "Would you like to install Chocolatey package manager? [y/n/q] " -NoNewline
+        Write-Host "Would you like to install Chocolatey package manager? [y/n/q] " -NoNewline
 
         $response = Read-Host
-        $response = $response.toLower()
+        $response = $response.ToLower()
 
         if ($response -eq "q") {
             Write-Host "Exiting..."
@@ -45,14 +42,14 @@ else {
         # TODO: Restart the console
     } elseif ($response -eq "n") {
         Write-Host "Please install VcXsrv manually."
-        Write-Host -NoNewLine "Press any key to continue..."
+        Write-Host -NoNewline "Press any key to continue..."
         $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
         exit 1
     }
 }
 
 # Install VcXsrv
-# TODO: Update path to include vcxsrv folder
+# TODO: Update path to incldue vcxsrv folder
 if (Get-Command "C:\Program Files\VcXsrv\xlaunch.exe" -ErrorAction SilentlyContinue) {
     Write-Host "VcXsrv is already installed."
 } else {
@@ -60,5 +57,5 @@ if (Get-Command "C:\Program Files\VcXsrv\xlaunch.exe" -ErrorAction SilentlyConti
 }
 
 # TODO: The following is for testing purposes only
-Write-Host -NoNewLine "Press any key to continue..."
+Write-Host -NoNewline "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
