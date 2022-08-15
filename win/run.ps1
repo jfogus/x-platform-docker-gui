0632ef27aa9eb4a30e14af23668c028c8ed7a338
Josh Fogus <josh.fogus@gmail.com>
2022-08-15 13:04:43 -0700

    Prevent running duplicate vxcsrv

--- win/run.ps1
+++ win/run.ps1
@@ -2,12 +2,16 @@ param([String]$container="")
 
 $vcxsrvPath ="C:\Program Files\VcXsrv"
 
-# TODO: Check if xlaunch is already running, no benefit to multiple xlaunch instances
 if ((Get-Command "${vcxsrvPath}\xlaunch" -ErrorAction SilentlyContinue) -and
     (Get-Command docker -ErrorAction SilentlyContinue)) {
-    # Update path and run the X11 environment
-    $env:path += ";${vcxsrvPath}"
-    xlaunch -run .\\win\\config.xlaunch
+    
+    if (Get-Process VcXsrv -ErrorAction SilentlyContinue) {
+        Write-Host "VcXsrv is already running."
+    } else {
+        # Update path and run the X11 environment
+        $env:path += ";${vcxsrvPath}"
+        xlaunch -run .\\win\\config.xlaunch
+    }
 
     # Run the Docker container
     # TODO: Programatically get the IP address
