5f77680a8e2fb282049ef8ad5b70fcace0741827
Josh Fogus <josh.fogus@gmail.com>
2022-08-16 13:46:23 -0700

    Added a script to uninstall dependencies on macOS.

---
+++ mac/uninstall.zsh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+. ./prompt.zsh
+
+# Uninstall XQuartz
+ynqPrompt "Uninstall XQuartz?"
+
+if [ $response = "q" ]; then
+    echo "Exiting..."
+    exit 1
+fi
+
+if [ $response = "y" ]; then
+    brew uninstall --cask xquartz
+else
+    echo "XQuartz was not uninstalled."
+fi
+
+# Uninstall Socat
+ynqPrompt "Uninstall Socat?"
+
+if [ $response = "q" ]; then
+    echo "Exiting..."
+    exit 1
+fi
+
+if [ $response = "y" ]; then
+    brew uninstall socat
+else
+    echo "Socat was not uninstalled."
+fi
+
+# Uninstall Brew
+ynqPrompt "Uninstall Brew?"
+
+if [ $response = "q" ]; then
+    echo "Exiting..."
+    exit 1
+fi
+
+if [ $response = "y" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+else
+    echo "Brew was not uninstalled."
+fi
