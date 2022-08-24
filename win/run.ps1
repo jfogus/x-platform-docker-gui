9de45573d2616c3be576feff2c993af7efe67b2c
Josh Fogus <josh.fogus@gmail.com>
2022-08-24 09:30:28 -0700

    Validates IP address input.

--- win/run.ps1
+++ win/run.ps1
@@ -1,6 +1,7 @@
 param([String]$container="")
 
 Import-Module -Name $PSScriptRoot\prompt.ps1
+Import-Module -Name $PSScriptRoot\validate.ps1
 
 $vcxsrvPath ="C:\Program Files\VcXsrv"
 
@@ -17,8 +18,9 @@ if ((Get-Command "${vcxsrvPath}\xlaunch" -ErrorAction SilentlyContinue) -and
     # Method of getting IP address from:
     # https://www.delftstack.com/howto/powershell/powershell-get-ip-address/#:~:text=variable%20in%20PowerShell.-,Use%20Get%2DNetIPAddress%20to%20Get%20IPv4%20Address%20Into%20a%20Variable,IP%20interfaces%20associated%20with%20addresses.
     # TODO: Returns 2 ip addresses when VirtualBox is installed. Try to get the correct one.
-    $ipAddress = openPrompt -Prompt "Please enter your local IP address"
+    # $ipAddress = openPrompt -Prompt "Please enter your local IP address"
     # $ipAddress = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress
+    $ipAddress = validatePrompt -Validator $function:validateIPv4Address -Prompt "Please enter your local IP address"
 
     # Start X server
     if (Get-Process VcXsrv -ErrorAction SilentlyContinue) {
